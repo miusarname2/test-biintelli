d4b8764a99ae7447ced723dba87bc73ef5f4f6db
miusarname2 <oscarmauricio631@gmail.com>
2024-03-09 09:26:41 -0500

    feat: :sparkles: Added the necessary configurations to use swagger both at startup and in the curler(s).

--- app/Journey/aplication/journey.routes.ts
+++ app/Journey/aplication/journey.routes.ts
@@ -1,9 +1,82 @@
 import { JourneyController } from "./journey.controller.js";
 import { Router,Response,Request } from "express";
+import { validateUser } from "../domain/storage/journey.js";
+import { limitGrt } from "../infrastructure/config/rateLimit.js";
 
-export const route:Router = Router();
+export const journeyRoute:Router = Router();
 
-route.get('/',async(req:Request|any,res:Response)=>{
+/**
+ * @swagger
+ * /journey:
+ *   post:
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Obtiene y consulta los vuelos o las rutas para hacer un viaje.
+ *     description: Obtiene y consulta los vuelos o las rutas para hacer un viaje, tiene un limite de 50000 consultas/hora.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               Origin:
+ *                 type: string
+ *                 description: Aeropuerto de origen.
+ *               Destination:
+ *                 type: string
+ *                 description: Aeropuerto de destino.
+ *     responses:
+ *       200:
+ *         description: Precios de viaje obtenidos exitosamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: ID único del viaje.
+ *                   Origin:
+ *                     type: string
+ *                     description: Aeropuerto de origen.
+ *                   Destination:
+ *                     type: string
+ *                     description: Aeropuerto de destino.
+ *                   price:
+ *                     type: string
+ *                     description: Precio del viaje.
+ *       400:
+ *         description: Datos de entrada inválidos.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   example: 400
+ *                 message:
+ *                   type: string
+ *                   example: Datos de entrada inválidos.
+ *       500:
+ *         description: Error interno del servidor al obtener precios de viaje.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   example: 500
+ *                 message:
+ *                   type: string
+ *                   example: Error al obtener precios de viaje.
+ */
+
+journeyRoute.post('/',limitGrt(),validateUser,async(req:Request|any,res:Response)=>{
     const resp = new JourneyController();
     res.send(await resp.GetController(req.body));
 })
--- index.ts
+++ index.ts
@@ -1,27 +1,68 @@
 import express, { Express, Request, Response } from "express";
-import { route } from "./app/Journey/aplication/journey.routes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import { journeyRoute } from "./app/Journey/aplication/journey.routes.js";
 import { crearToken,validarToken } from "./auth/jwt.js";
 import morgan from 'morgan'
 import cors from 'cors';
 import dotenv from "dotenv";
 
 // Environment variables
-dotenv.config();
+dotenv.config({path:"./"});
 
+//Init server
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// Adding Middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({origin:"*"}))
 
+// Config Swagger
+
+// Swagger config
+const options = {
+  swaggerDefinition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Flight Manager",
+      version: "1.0.0",
+      description: "El backend de la aplicación de gestión de vídeos ofrece servicios para la administración de usuarios, gestión de vídeos y autenticación. Incluye funcionalidades como CRUD de usuarios y vídeos, autenticación segura, recuperación de vídeos por usuario, acceso a vídeos públicos y privados, lista de vídeos mejor calificados, y endpoints adicionales como búsqueda por palabras clave y gestión de comentarios. En resumen, proporciona una API segura para operaciones relacionadas con usuarios, vídeos y características clave de la aplicación.",
+    },
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        }
+      }
+    },
+  },
+  apis: ["index.ts","./auth/jwt.ts","./app/Journey/aplication/journey.routes.ts"], // Ruta a los archivos donde se definen las rutas de la API
+};
+
+// Init swagger
+
+const specs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+
+// Define routes
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server ⚡️");
 });
 
-app.get('/tk',crearToken);
-app.use("/user",route);
+app.post('/tk',crearToken);
+app.use("/journey",validarToken,journeyRoute);
 
+//Listen port
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
